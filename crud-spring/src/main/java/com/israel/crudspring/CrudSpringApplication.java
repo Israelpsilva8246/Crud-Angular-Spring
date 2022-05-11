6da7324d09f1c3f872fbdc9c018d4f232a5d9f7f
Israel Pontes <israel.pontes.1920@gmail.com>
2022-05-11 09:26:47 -0300

    Crud Spring

---
+++ crud-spring/src/main/java/com/israel/crudspring/CrudSpringApplication.java
@@ -0,0 +1,31 @@
+package com.israel.crudspring;
+
+import com.israel.crudspring.model.Cliente;
+import com.israel.crudspring.repository.ClienteRepository;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initialDatabase(ClienteRepository clienteRepository) {
+		return args -> {
+			clienteRepository.deleteAll();
+			Cliente c = new Cliente();
+			c.setNome("Israel");
+			c.setSobrenome("Pontes");
+			c.setIdade("20");
+			c.setDataNascimento("05/06/2001");
+			clienteRepository.save(c);
+		};
+	}
+
+}
